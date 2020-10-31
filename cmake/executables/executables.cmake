fb5cd0976d778b2816d27968dfa60ac183070235
boristestov <boris.testov@gmail.com>
2020-10-31 23:27:33 +0300

    cmake changed

--- cmake/executables/executables.cmake
+++ cmake/executables/executables.cmake
@@ -1,9 +1,10 @@
 include_directories(${INCLUDE_DIR})
 
-file (GLOB SOURCES "${SRC_DIR}/*.cpp")
-file (GLOB HEADERS "${INCLUDE_DIR}/*.h" "${INCLUDE_DIR}/*.hpp")
+file(GLOB SOURCES "${SRC_DIR}/*.cpp")
+file(GLOB HEADERS "${INCLUDE_DIR}/*.h" "${INCLUDE_DIR}/*.hpp")
 
 add_executable(${PROJECT_NAME} ${SOURCES} ${HEADERS})
+target_link_libraries(${PROJECT_NAME} pthread)
 set_target_properties(${PROJECT_NAME} PROPERTIES ${TARGET_PROPERTIES})
 target_compile_options(${PROJECT_NAME} PRIVATE ${COMPILE_OPTIONS})
 
